68c9880301b41ad15d923344a4f64a428e209454
parth <parthaahire2006@gmail.com>
2026-09-05 19:56:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                for (int k = j + 1; k < nums.length; k++) {
+
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (sum == 0) {
+
+                        List<Integer> temp = Arrays.asList(
+                            nums[i], nums[j], nums[k]
+                        );
+
+                        Collections.sort(temp);
+
+                        if (!result.contains(temp)) {
+                            result.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
